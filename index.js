dfb9131551e4c02079e0e9911d50f4560878522a
wahome <wahomewgitonga@gmail.com>
2024-03-23 15:05:38 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 10;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 20, 15);
